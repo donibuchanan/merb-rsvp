3fdb664cd2438ce949d613db1c5e179588272e79
donibuchanan <don_i_buchanan@gmail.com>
2009-08-09 19:33:56 +1000

    most of the layout is done now

--- app/helpers/invites_helper.rb
+++ app/helpers/invites_helper.rb
@@ -1,18 +1,14 @@
 
 module Merb
   module InvitesHelper
-    def accept_message(accepted)
-      state = accepted ? "accepted" : "declined"
-      " has #{state} the invite"
+    def yes_class(invite)
+      "selected" if invite.accepted
+    end
+    def no_class(invite)
+      "selected" unless invite.accepted
     end
   end
-  module InviteIncludes
-    include InvitesHelper
-    def accepted_message
-      return "" if accepted_at.nil? and declined_at.nil?
-      "#{name}" + accept_message(accepted)
-    end
-  end
+  
 end # Merb
 
-Invite.send(:include, Merb::InviteIncludes)
+
