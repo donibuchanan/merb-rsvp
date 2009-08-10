3add8ed90aa3dfc5a30c8d68bb25898bb0be7464
donibuchanan <don_i_buchanan@gmail.com>
2009-08-11 09:17:44 +1000

    restored production env

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,9 +1,9 @@
 Merb.logger.info("Loaded PRODUCTION Environment...")
 Merb::Config.use { |c|
   c[:exception_details] = false
-  c[:reload_templates] = true
-  c[:reload_classes] = true
+  c[:reload_classes] = false
   c[:log_level] = :error
+  
   c[:log_file]  = Merb.root / "log" / "production.log"
   # or redirect logger using IO handle
   # c[:log_stream] = STDOUT
